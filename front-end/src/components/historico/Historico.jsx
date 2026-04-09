56f4304831f9a423fc219bc032c158ed0fcb7aa5
Taylan <taylansilva0402@gmail.com>
2026-04-09 19:54:06 -0300

    feat: estilizando tabela de consultas

--- front-end/src/components/historico/Historico.jsx
+++ front-end/src/components/historico/Historico.jsx
@@ -1,28 +1,38 @@
 import styles from './Historico.module.css'
 
 export default function Historico({historico}) {
+
+    if (historico.length === 0) {
     return (
-        <table>
-            <thead>
-                <tr className={styles.header}>
-                    <th>Cliente</th>
-                    <th>Valor</th>
-                    <th>Desconto</th>
-                    <th>Cashback</th>
-                    <th>Data</th>
-                </tr>
-            </thead>
-            <tbody className={styles.corpoTabela}>
-                {historico.map((item, index) => (
-                <tr className={styles.celula} key={index}>
-                    <td>{item.tipo_cliente}</td>
-                    <td>R$ {item.valor}</td>
-                    <td>{item.desconto}%</td>
-                    <td>R$ {item.cashback}</td>
-                    <td>{item.data}</td>
-                </tr>
-                ))}
-            </tbody>
-        </table>
+      <div className={styles.Empty}>
+        Nenhuma consulta realizada ainda
+      </div>
+    )}
+
+    return (
+        <div className={styles.TableWrapper}>
+            <table className={styles.Tabela}>
+                <thead>
+                    <tr className={styles.Header}>
+                        <th>Cliente</th>
+                        <th>Valor</th>
+                        <th>Desconto</th>
+                        <th>Cashback</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody className={styles.CorpoTabela}>
+                    {historico.map((item, index) => (
+                    <tr className={styles.celula} key={index}>
+                        <td>{item.tipo_cliente}</td>
+                        <td>R$ {item.valor}</td>
+                        <td>{item.desconto}%</td>
+                        <td>R$ {item.cashback}</td>
+                        <td>{item.data}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
