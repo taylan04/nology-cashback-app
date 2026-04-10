8a04486670591b891c5ba661b670df1775b70b3a
Taylan <taylansilva0402@gmail.com>
2026-04-09 22:26:50 -0300

    fix: refatorando classe de histórico

--- back-end/app/models/historico_model.py
+++ back-end/app/models/historico_model.py
@@ -1,26 +1,19 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from datetime import datetime
 
 Base = declarative_base()
 
-class Produto(Base):
+class Historico(Base):
     __tablename__ = "historico"
 
-    id_consulta = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key = True, index = True)
     ip = Column(String)
     tipo_cliente = Column(String)
     valor = Column(Float)
     desconto = Column(Float)
-    data_criacao = Column(DateTime)
-
-    def __init__(self, id_consulta, ip, tipo_cliente, valor, desconto):
-        self.id_consulta = id_consulta
-        self.ip = ip
-        self.tipo_cliente = tipo_cliente
-        self.desconto = desconto
-        self.valor = valor
-        self.data_criacao = datetime.strftime("%Y-%m-%d %H:%M:%S")
+    cashback = Column(Float)
+    data_criacao = Column(DateTime, default = datetime.now)
 
     def __str__(self):
-        return f'{self.id_consulta}, {self.ip}, {self.tipo_cliente}, {self.desconto}, {self.valor}, {self.data_criacao}'
+        return f'{self.id}, {self.ip}, {self.tipo_cliente}, {self.valor}, {self.desconto}, {self.cashback}, {self.data_criacao}'
